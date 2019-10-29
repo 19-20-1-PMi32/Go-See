eebdca1726ee33e1f9a6bfbfad1e06c10459c762
Den <denny1301@gmail.com>
2019-10-30 01:07:59 +0200

    Created service for user authentication

---
+++ Go-See/GS.BusinessLogic/Authentication.cs
@@ -0,0 +1,57 @@
+﻿using GS.BusinessLogic.Contracts;
+using GS.DataBase;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GS.BusinessLogic
+{
+    public class Authentication : IAuthentication
+    {
+        private UnitOfWork unitOfWork;
+
+        public Authentication(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public Guid CreateUser(GS.Core.DTO.User userParam)
+        {
+            Guid id = Guid.NewGuid();
+
+            var user = new GS.DataBase.Entities.User
+            {
+                Id = id,
+                Login = userParam.Login,
+                FirstName = userParam.FirstName,
+                LastName = userParam.LastName,
+                Email = userParam.Email,
+                Phone = userParam.Phone,
+                PasswordHash = userParam.PasswordHash
+            };
+
+            unitOfWork.UserRepository.Create(user);
+            unitOfWork.Commit();
+
+            return id;
+        }
+
+        public async Task<bool> LogIn(string username, string password)
+        {
+            IEnumerable<GS.DataBase.Entities.User> users = await unitOfWork.UserRepository.GetAllAsync();
+
+            GS.DataBase.Entities.User user = users.Where(entry => entry.Login == username).First();
+            
+            if (user.PasswordHash == password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Go-See/GS.BusinessLogic/Contracts/IAuthentication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GS.BusinessLogic.Contracts
+{
+    interface IAuthentication
+    {
+        Task<bool> LogIn(string username, string password);
+        Guid CreateUser(GS.Core.DTO.User userParam);
+    }
+}
