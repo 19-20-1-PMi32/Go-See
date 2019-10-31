2821c166845bb01f236b50456bd20b95ef0e833c
Sofiia Romah <sofija.romah@gmail.com>
2019-10-31 23:00:06 +0200

    Added tests to city, place, review

--- Go-See/Tests/GS.DataBase.Test/PlaceRepositoryTest.cs
+++ Go-See/Tests/GS.DataBase.Test/PlaceRepositoryTest.cs
@@ -9,18 +9,34 @@ using Xunit;
 
 namespace GS.DataBaseTest
 {
-    public class PlaceRepositoryTest: TestBase
+    public class PlaceRepositoryTest
     {
-        public PlaceRepositoryTest() : base() { }
+
+        public PlaceRepositoryTest() { }
+        
+        public UnitOfWork UnitOfWork { get; set; }
+        
+
+        public UnitOfWork GetOption(string name)
+        {
+            var options = new DbContextOptionsBuilder<GSDbContext>()
+                .UseInMemoryDatabase(databaseName: name)
+                .Options;
+
+            var context = new GSDbContext(options);
+
+            UnitOfWork = new UnitOfWork(context);
+
+            return UnitOfWork;
+
+        }
 
         [Fact]
         public async Task Get()
         {
-            var options = new DbContextOptionsBuilder<GSDbContext>()
-                .UseInMemoryDatabase(databaseName: "Test")
-                .Options;
-            var context = new GSDbContext(options);
-            UnitOfWork = new UnitOfWork(context);
+
+            UnitOfWork = GetOption("TestGetPlace");
+
 
             var expectedPlace = new DataBase.Entities.Place()
             {
@@ -42,7 +58,9 @@ namespace GS.DataBaseTest
             Assert.Equal(expectedPlace.Name, place.Name);
 
             Assert.Equal(expectedPlace.CityId, place.CityId);
+
             Assert.Equal(expectedPlace.Type, place.Type);
+
             Assert.Equal(expectedPlace.Description, place.Description);
 
         }
@@ -50,13 +68,9 @@ namespace GS.DataBaseTest
         [Fact]
         public async Task Create()
         {
-            var options = new DbContextOptionsBuilder<GSDbContext>()
-                .UseInMemoryDatabase(databaseName: "Test")
-                .Options;
 
-            var context = new GSDbContext(options);
+            UnitOfWork = GetOption("TestCreatePlace");
 
-            UnitOfWork = new UnitOfWork(context);
 
             var expectedPlace = new DataBase.Entities.Place()
             {
@@ -88,13 +102,9 @@ namespace GS.DataBaseTest
         [Fact]
         public async Task GetAll()
         {
-            var options = new DbContextOptionsBuilder<GSDbContext>()
-                .UseInMemoryDatabase(databaseName: "Test")
-                .Options;
 
-            var context = new GSDbContext(options);
+            UnitOfWork = GetOption("TestGetAllPlace");
 
-            UnitOfWork = new UnitOfWork(context);
 
             var places = new List<DataBase.Entities.Place>()
             {
@@ -115,7 +125,9 @@ namespace GS.DataBaseTest
                     Type = "architecture",
                     Description = "Saint Sophia’s Cathedral is an outstanding complex; "
                 }
+
             };
+
 
             UnitOfWork.PlaceRepository.Create(places[0]);
 
@@ -127,20 +139,16 @@ namespace GS.DataBaseTest
 
             List<DataBase.Entities.Place> place = test.ToList();
 
-            Assert.Equal(place.Count(), 2);
+            Assert.Equal(2, place.Count());
 
         }
 
         [Fact]
         public async Task Update()
         {
-            var options = new DbContextOptionsBuilder<GSDbContext>()
-                .UseInMemoryDatabase(databaseName: "Test")
-                .Options;
 
-            var context = new GSDbContext(options);
+            UnitOfWork = GetOption("TestUpdatePlace");
 
-            UnitOfWork = new UnitOfWork(context);
 
             var expectedPlace = new DataBase.Entities.Place()
             {
@@ -163,6 +171,51 @@ namespace GS.DataBaseTest
 
             Assert.Equal(expectedPlace.CityId, test.CityId);
 
+        }
+
+        [Fact]
+        public async Task Delete()
+        {
+
+            UnitOfWork = GetOption("TestDeletePlace");
+
+
+            var places = new List<DataBase.Entities.Place>()
+            {
+                new DataBase.Entities.Place()
+                {
+                    Id = 1,
+                    Name = "Saint Sophia's Cathedral",
+                    CityId = 1,
+                    Type = "architecture",
+                    Description = "Saint Sophia’s Cathedral is an outstanding complex; "
+                },
+
+                new DataBase.Entities.Place()
+                {
+                     Id = 2,
+                    Name = "Saint Sophia's Cathedral",
+                    CityId = 2,
+                    Type = "architecture",
+                    Description = "Saint Sophia’s Cathedral is an outstanding complex; "
+                }
+
+            };
+
+            UnitOfWork.PlaceRepository.Create(places[0]);
+
+            UnitOfWork.PlaceRepository.Create(places[1]);
+
+            UnitOfWork.PlaceRepository.Delete(1);
+
+            UnitOfWork.Commit();
+
+            var test = await UnitOfWork.PlaceRepository.GetAll();
+
+            List<DataBase.Entities.Place> place = test.ToList();
+
+            Assert.Equal(1, place.Count());
+                   
         }
     }
 }
--- Go-See/Tests/GS.DataBase.Test/ReviewRepositoryTest.cs
+++ Go-See/Tests/GS.DataBase.Test/ReviewRepositoryTest.cs
@@ -1,10 +1,216 @@
-﻿using System;
+﻿using GS.DataBase;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace GS.DataBaseTest
 {
-    class ReviewRepositoryTest: TestBase
+    public class ReviewRepositoryTest
     {
+
+        public ReviewRepositoryTest() { }
+
+        public UnitOfWork UnitOfWork { get; set; }
+
+        public UnitOfWork GetOption(string name)
+        {
+            var options = new DbContextOptionsBuilder<GSDbContext>()
+                .UseInMemoryDatabase(databaseName: name)
+                .Options;
+
+            var context = new GSDbContext(options);
+
+            UnitOfWork = new UnitOfWork(context);
+
+            return UnitOfWork;
+
+        }
+
+        [Fact]
+        public async Task Get()
+        {
+
+            UnitOfWork = GetOption("TestGetReview");
+
+            var expectedReview = new DataBase.Entities.Review()
+            {
+                Id = 1,
+                UserId = "Jack",
+                PlaceId = 1,
+                Rating = 4,
+                Text = "This is picturesque cathedral, I love it"
+            };
+
+            UnitOfWork.ReviewRepository.Create(expectedReview);
+
+            UnitOfWork.Commit();
+
+            var review = await UnitOfWork.ReviewRepository.Get(1);
+
+            Assert.Equal(expectedReview.Id, review.Id);
+
+            Assert.Equal(expectedReview.UserId, review.UserId);
+
+            Assert.Equal(expectedReview.PlaceId, review.PlaceId);
+
+            Assert.Equal(expectedReview.Rating, review.Rating);
+
+            Assert.Equal(expectedReview.Text, review.Text);
+
+        }
+
+        [Fact]
+        public async Task Create()
+        {
+
+            UnitOfWork = GetOption("TestGetReview");
+
+            var expectedReview = new DataBase.Entities.Review()
+            {
+                Id = 1,
+                UserId = "Jack",
+                PlaceId = 1,
+                Rating = 4,
+                Text = "This is picturesque cathedral, I love it"
+            };
+
+            UnitOfWork.ReviewRepository.Create(expectedReview);
+
+            UnitOfWork.Commit();
+
+            var review = await UnitOfWork.ReviewRepository.Get(1);
+
+            Assert.Equal(expectedReview.Id, review.Id);
+
+            Assert.Equal(expectedReview.UserId, review.UserId);
+
+            Assert.Equal(expectedReview.PlaceId, review.PlaceId);
+
+            Assert.Equal(expectedReview.Rating, review.Rating);
+
+            Assert.Equal(expectedReview.Text, review.Text);
+
+        }
+
+        [Fact]
+        public async Task GetAll()
+        {
+
+            UnitOfWork = GetOption("TestGetAllReview");
+
+
+            var reviews = new List<DataBase.Entities.Review>()
+            {
+                new DataBase.Entities.Review()
+                {
+                    Id = 1,
+                    UserId = "Jack",
+                    PlaceId = 1,
+                    Rating = 4,
+                    Text = "This is picturesque cathedral, I love it"
+                },
+
+                new DataBase.Entities.Review()
+                {
+                    Id = 2,
+                    UserId = "Leo",
+                    PlaceId = 2,
+                    Rating = 5,
+                    Text = "Amazing place!"
+                }
+            };
+
+            UnitOfWork.ReviewRepository.Create(reviews[0]);
+
+            UnitOfWork.ReviewRepository.Create(reviews[1]);
+
+            UnitOfWork.Commit();
+
+            var test = await UnitOfWork.ReviewRepository.GetAll();
+
+            List<DataBase.Entities.Review> review = test.ToList();
+
+            Assert.Equal(2, review.Count());
+
+        }
+
+        [Fact]
+        public async Task Update()
+        {
+
+            UnitOfWork = GetOption("TestUpdateReview");
+
+
+            var expectedReview = new DataBase.Entities.Review()
+            {
+                Id = 1,
+                UserId = "Jack",
+                PlaceId = 1,
+                Rating = 4,
+                Text = "This is picturesque cathedral, I love it"
+            };
+
+            UnitOfWork.ReviewRepository.Create(expectedReview);
+
+            UnitOfWork.Commit();
+
+            expectedReview.Id = 4;
+
+            UnitOfWork.ReviewRepository.Update(expectedReview);
+
+            var test = await UnitOfWork.ReviewRepository.Get(1);
+
+            Assert.Equal(expectedReview.Id, test.Id);
+
+        }
+
+        [Fact]
+        public async Task Delete()
+        {
+
+            UnitOfWork = GetOption("TestDeleteReview");
+
+
+            var reviews = new List<DataBase.Entities.Review>()
+            {
+                new DataBase.Entities.Review()
+                {
+                    Id = 1,
+                    UserId = "Jack",
+                    PlaceId = 1,
+                    Rating = 4,
+                    Text = "This is picturesque cathedral, I love it"
+                },
+
+                new DataBase.Entities.Review()
+                {
+                    Id = 2,
+                    UserId = "Leo",
+                    PlaceId = 2,
+                    Rating = 5,
+                    Text = "Amazing place!"
+                }
+            };
+
+            UnitOfWork.ReviewRepository.Create(reviews[0]);
+
+            UnitOfWork.ReviewRepository.Create(reviews[1]);
+
+            UnitOfWork.ReviewRepository.Delete(1);
+
+            UnitOfWork.Commit();
+
+            var test = await UnitOfWork.ReviewRepository.GetAll();
+
+            List<DataBase.Entities.Review> review = test.ToList();
+
+            Assert.Equal(1, review.Count());
+
+        }
+
     }
 }
