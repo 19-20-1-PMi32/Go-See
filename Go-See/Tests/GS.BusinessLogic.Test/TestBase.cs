954cdcbb6e3d4ddbaac8e2bda62b42e135d085b0
Oleksandr Zelinskyi <sashko.zel2000@gmail.com>
2019-11-03 00:13:32 +0200

    added AuthenticationService tests

---
+++ Go-See/Tests/GS.BusinessLogic.Test/TestBase.cs
@@ -0,0 +1,27 @@
+﻿using GS.DataBase;
+using GS.DataBase.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GS.BusinessLogic.Test
+{
+    public abstract class TestBase
+    {
+        protected readonly UnitOfWork _unitOfWork;
+
+        protected abstract string ContextDBName { get; }
+
+        protected TestBase()
+        {
+            var options = new DbContextOptionsBuilder<GSDbContext>()
+                .UseInMemoryDatabase(databaseName: ContextDBName)
+                .Options;
+
+            var context = new GSDbContext(options, false);
+
+            _unitOfWork = new UnitOfWork(context);
+        }
+    }
+}
