76269f592108efaac1bf4b0e07a1b3ee559afaf0
Den <denny1301@gmail.com>
2019-11-12 13:46:52 +0200

    Added automapper profiles

---
+++ Go-See/GS.BusinessLogic/EntityDtoProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GS.BusinessLogic
+{
+    public class EntityDtoProfile : Profile
+    {
+        public EntityDtoProfile()
+        {
+            CreateMap<DataBase.Entities.City, Core.DTO.City>();
+            CreateMap<DataBase.Entities.Place, Core.DTO.Place>();
+            CreateMap<DataBase.Entities.Review, Core.DTO.Review>();
+            CreateMap<DataBase.Entities.Trip, Core.DTO.Trip>();
+            CreateMap<DataBase.Entities.TripNode, Core.DTO.TripNode>();
+            CreateMap<DataBase.Entities.User, Core.DTO.User>();
+        }
+    }
+}
---
+++ Go-See/GS.WebAPI/ParametersDtoProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GS.Core.DTO;
+using GS.WebAPI.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GS.WebAPI
+{
+    public class ParametersDtoProfile : Profile
+    {
+        public ParametersDtoProfile()
+        { 
+            CreateMap<UserParam, User>();
+        }
+    }
+}
