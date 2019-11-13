f019795a99148e5f46a1ad864b542c60ee270d4f
Den <denny1301@gmail.com>
2019-11-13 10:45:01 +0200

    Added methods for field update to user service

--- Go-See/GS.BusinessLogic/Contracts/IUserService.cs
+++ Go-See/GS.BusinessLogic/Contracts/IUserService.cs
@@ -8,6 +8,18 @@ namespace GS.BusinessLogic.Contracts
 {
     public interface IUserService
     {
-        Task<User> GetUser(Guid id); 
+        Task<User> GetUser(Guid userId);
+
+        Task UpdateLogin(Guid userId, string newLogin);
+
+        Task UpdateFirstName(Guid userId, string newFirstName);
+
+        Task UpdateLastName(Guid userId, string newLastName);
+
+        Task UpdateEmail(Guid userId, string newEmail);
+
+        Task UpdatePhone(Guid userId, string newPhone);
+
+        Task UpdatePasswordHash(Guid userId, string newPasswordHash);
     }
 }
--- Go-See/GS.BusinessLogic/Services/UserService.cs
+++ Go-See/GS.BusinessLogic/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using GS.BusinessLogic.Contracts;
+﻿using AutoMapper;
+using GS.BusinessLogic.Contracts;
 using GS.Core.DTO;
 using GS.DataBase;
 using System;
@@ -11,10 +12,12 @@ namespace GS.BusinessLogic
     public class UserService : IUserService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public UserService(IUnitOfWork unitOfWork)
+        public UserService(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
         public async Task<User> GetUser(Guid userId)
@@ -23,18 +26,105 @@ namespace GS.BusinessLogic
 
             if (userEntity != null)
             {
-                var user = new User
-                {
-                    Id = userEntity.Id,
-                    FirstName = userEntity.FirstName,
-                    LastName = userEntity.LastName,
-                    Email = userEntity.Email,
-                    Phone = userEntity.Phone,
-                    Login = userEntity.Login,
-                    PasswordHash = userEntity.PasswordHash
-                };
+                var user = _mapper.Map<User>(userEntity);
 
                 return user;
+            }
+            else
+            {
+                throw new ArgumentException("Can not find user with such id");
+            }
+        }
+
+        public async Task UpdateLogin(Guid userId, string newLogin)
+        {
+            var userEntity = await _unitOfWork.UserRepository.Get(userId);
+
+            if (userEntity != null)
+            {
+                userEntity.Login = newLogin;
+                _unitOfWork.UserRepository.Update(userEntity);
+                await _unitOfWork.Commit();
+            }
+            else
+            {
+                throw new ArgumentException("Can not find user with such id");
+            }
+        }
+
+        public async Task UpdateFirstName(Guid userId, string newFirstName)
+        {
+            var userEntity = await _unitOfWork.UserRepository.Get(userId);
+
+            if (userEntity != null)
+            {
+                userEntity.FirstName = newFirstName;
+                _unitOfWork.UserRepository.Update(userEntity);
+                await _unitOfWork.Commit();
+            }
+            else
+            {
+                throw new ArgumentException("Can not find user with such id");
+            }
+        }
+
+        public async Task UpdateLastName(Guid userId, string newLastName)
+        {
+            var userEntity = await _unitOfWork.UserRepository.Get(userId);
+
+            if (userEntity != null)
+            {
+                userEntity.LastName = newLastName;
+                _unitOfWork.UserRepository.Update(userEntity);
+                await _unitOfWork.Commit();
+            }
+            else
+            {
+                throw new ArgumentException("Can not find user with such id");
+            }
+        }
+
+        public async Task UpdateEmail(Guid userId, string newEmail)
+        {
+            var userEntity = await _unitOfWork.UserRepository.Get(userId);
+
+            if (userEntity != null)
+            {
+                userEntity.Email = newEmail;
+                _unitOfWork.UserRepository.Update(userEntity);
+                await _unitOfWork.Commit();
+            }
+            else
+            {
+                throw new ArgumentException("Can not find user with such id");
+            }
+        }
+
+        public async Task UpdatePhone(Guid userId, string newPhone)
+        {
+            var userEntity = await _unitOfWork.UserRepository.Get(userId);
+
+            if (userEntity != null)
+            {
+                userEntity.Phone = newPhone;
+                _unitOfWork.UserRepository.Update(userEntity);
+                await _unitOfWork.Commit();
+            }
+            else
+            {
+                throw new ArgumentException("Can not find user with such id");
+            }
+        }
+
+        public async Task UpdatePasswordHash(Guid userId, string newPasswordHash)
+        {
+            var userEntity = await _unitOfWork.UserRepository.Get(userId);
+
+            if (userEntity != null)
+            {
+                userEntity.PasswordHash = newPasswordHash;
+                _unitOfWork.UserRepository.Update(userEntity);
+                await _unitOfWork.Commit();
             }
             else
             {
